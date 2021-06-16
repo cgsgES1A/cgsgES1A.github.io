807b95587540946db141c9189ccd2161de773748
ES1A <kat22@mail.ru>
2021-06-16 14:00:45 +0300

    WIP

--- express/index.js
+++ express/index.js
@@ -5,16 +5,38 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+let cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
+
+let nick = "";
+
+app.get('/cookie', (req, res) => {
+    if (req.method === 'POST') {
+        var data = '';
+
+        req.on('data', chunk => {
+            data += chunk;
+        });
+        req.on('end', () => {
+            nick = data;
+        });
+    }
+});
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/server2.html');
+    res.cookie("nickname", nick);
 });
 
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
+        io.emit('chat message', nick + msg);
     });
 });
 
-server.listen(8000, () => {
+server.listen(8000, (err) => {
+    if (err)
+        throw err;
     console.log('listening on *:8000');
 });
